a0dde07fdde207cbd133899081bbbc7692c8d479
Daniel O <danielogunlana23@gmail.com>
2021-07-29 00:13:08 -0400

    added function to add color to time blocks

--- script.js
+++ script.js
@@ -9,3 +9,23 @@ function displayTime(){
 setInterval(displayTime,1000);
 
 //Function to colour code the time blocks.
+function colourCodeBlocks() {
+
+  $('.row').each(function() {
+      
+      var time = moment().format('H');
+
+      var block =  $(this).attr('id');
+  
+      if (block == time) {
+          $(this).children().eq(1).addClass('present');
+      } else if (block < time) {
+          $(this).children().eq(1).addClass('past');
+      } else {
+          $(this).children().eq(1).addClass('future');
+      }
+  })
+};
+
+colourCodeBlocks();
+
